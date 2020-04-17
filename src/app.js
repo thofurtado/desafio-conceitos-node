ff0e513c4ee31fb3e4283ff3641c9f66d2937dd9
thofurtado <32690619+thofurtado@users.noreply.github.com>
2020-04-16 21:33:17 -0300

    adicionado o get e o post
    adicionado o get e o post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,11 +11,30 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    // const {title} =  req.query;
+
+    // const results = title
+    // ? repositories.filter(repository => repository.title.includes(title))
+    // : repositories;
+    
+    // return res.json(results);
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, techs} =  request.body;
+  
+  const repository = {
+    id: uuid(),
+    title, 
+    url:'https://github.com/thofurtado/desafio-conceitos-node',
+    techs,
+    likes:0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
